94403274c32de46ad334d14280b862f585e071da
Leydi-Aro <89089568+Leydi-Aro@users.noreply.github.com>
2023-11-08 11:56:26 -0500

    login and registration design
    feat: login and registration design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RegisterComponent} from "./pages/register/register.component";
+import {LoginComponent} from "./pages/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo:'navigation/login', pathMatch:'full'},
+  {path: 'navigation/login', component: LoginComponent},
+  {path: 'navigation/register', component: RegisterComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ToolbarComponent } from './pages/toolbar/toolbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ToolbarComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatCardModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
